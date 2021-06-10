475557f65222b254d411c0b3aeac2a0c320774a7
Flowers Safety <40meta@gmail.com>
2021-06-10 20:29:09 +0600

    algorithm for which bot to run was implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
 import System.Environment (getArgs)
-import Vk (startBotWithLogger)
+import qualified Tg
+import qualified Vk
 
 main :: IO ()
-main = getArgs >>= startBotWithLogger
+main = getArgs
+    >>= \ args ->
+        let botToRun = if length args == 4
+            then Tg.startBotWithLogger
+            else Vk.startBotWithLogger
+        in botToRun args
